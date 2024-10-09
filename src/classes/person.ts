576a427a3c605baa76c95864bc2ecbb8932d3ac7
Joao Murilo Pereira de Oliveira <j.murilo.mobile@gmail.com>
2024-10-09 15:55:08 -0300

    primeiro commit

---
+++ src/classes/person.ts
@@ -0,0 +1,9 @@
+class person {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+export default person;
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import person from "./classes/person";
+
+const p = new person("Bianca");
+
+console.log(p.name);
+
+alert("Voce foi alertado !");
+
+
